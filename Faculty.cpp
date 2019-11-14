69b415d85d60c87ba3e090964961240c77704583
Matt Nwerem <MattNwerem@Matts-MacBook-Pro-5.local>
2019-11-14 14:20:42 -0800

    faculty

---
+++ Faculty.cpp
@@ -0,0 +1,48 @@
+// #include "Faculty.h"
+//
+// Faculty::Faculty()
+// {
+//   name = " ";
+//   IDnumber = 0;
+//   levelField = " ";
+//   department = " ";
+// //  studentIDs = " ";
+// }
+// Faculty::Faculty(string facName, int idNum , string levelF, string depart, LinkedList<int> studIDs)
+// {
+//   name = facName;
+//   IDnumber = idNum;
+//   levelField = levelF;
+//   department = depart;
+//   studentIDs = studIDs;
+// }
+//
+// Faculty::~Faculty()
+// {
+//
+// }
+//
+// string Faculty::getDepartment()
+// {
+//   return department;
+// }
+//
+// double Faculty::getStudentIDs()
+// {
+//   return studentIDs;
+// }
+// int Faculty::getAdvisorID()
+// {
+//   return advisorID;
+// }
+//
+//
+// void Faculty::getInfo()
+// {
+//   cout << "--Faculty Information--" << endl;
+//   cout << "Name: " << getName() << endl;
+//   cout << "ID Number: " << getID() << endl;
+//   cout << "Level: " << getLevelField() << endl;
+//   cout << "Department: " << getDepartment() << endl;
+//   cout << "Students Advising: " << getStudentIDs() << endl;
+// }
---
+++ Faculty.h
@@ -0,0 +1,20 @@
+// #include "Person.h"
+//
+// using namespace std;
+// // Student publicly inherits from Person
+// class Faculty: public Person
+// {
+//   public:
+//     Faculty();
+//   //  Faculty(string facName, int idNum , string levelF, string depart, LinkedList<int> studIDs);
+//     ~Faculty();
+//     string getDepartment();
+//   //  LinkedList<int> getStudentIDs();
+//     void getInfo();
+//
+//
+//   private:
+//     string department;
+//     //LinkedList<int> studentIDs;
+//
+// };
